6114cc97b9696a0537f1bbab988b6803c60e6e3d
zebra <usovnakarantine@gmail.com>
2025-04-22 18:34:04 +0300

    Update MainConfig.java

--- src/main/java/com/example/projectdemex/config/MainConfig.java
+++ src/main/java/com/example/projectdemex/config/MainConfig.java
@@ -105,3 +105,22 @@ public class MainConfig {
 Cannot resolve 'surname'
 Cannot resolve 'username'
 */
+/*
+<div class="mb-3">
+    <label for="name" class="form-label">Имя:</label>
+    <input class="form-control" type="text" placeholder="Введите имя" th:field="*{name}" id="name" aria-describedby="nameHelp"/>
+    <div id="nameHelp" class="form-text">Примеры имени: Владислав, Олег, John, Joe</div>
+    <span th:if="${#fields.hasErrors('name')}" th:errors="*{name}" class="text-danger"></span>
+</div>
+<div class="mb-3">
+    <label for="surname" class="form-label">Фамилия:</label>
+    <input class="form-control" type="text" placeholder="Введите фамилию" th:field="*{surname}" id="surname" aria-describedby="surnameHelp"/>
+    <div id="surnameHelp" class="form-text">Примеры фамилии: Иванов, Петров, Gonzales, Alvarez</div>
+    <span th:if="${#fields.hasErrors('surname')}" th:errors="*{surname}" class="text-danger"></span>
+</div>
+<div class="mb-3">
+    <label for="username_reg" class="form-label">Логин:</label>
+    <input class="form-control" type="text" placeholder="Введите логин" th:field="*{username}" id="username_reg" aria-describedby="usernameHelp"/>
+    <span th:if="${#fields.hasErrors('username')}" th:errors="*{username}" class="text-danger"></span>
+    <div id="usernameHelp" class="form-text">Примеры логина: лимон1724, atomic2020, Самурай11</div>
+</div>*/
